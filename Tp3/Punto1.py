3bc407852da7fe53881aefee230641a24f3126b6
xxMeDuzaa <mariamelinaduarte@gmail.com>
2026-04-22 21:16:50 -0300

    punto1

---
+++ Tp3/Punto1.py
@@ -0,0 +1,40 @@
+import threading
+
+class SingletonFactorial:
+    _instancia = None
+    _lock = threading.Lock()
+    _memoria = {}
+
+    def new(cls):
+        if cls._instancia is None:
+            with cls._lock:
+                if cls._instancia is None:
+                    cls._instancia = super().new(cls)
+        return cls._instancia
+
+    def factorial(self, n):
+        if n < 0:
+            raise ValueError("Factorial no definido para negativos")
+
+        if n in self._memoria:
+            return self._memoria[n]
+
+        with self._lock:
+            if n in self._memoria:
+                return self._memoria[n]
+
+            if n == 0 or n == 1:
+                resultado = 1
+            else:
+                resultado = 1
+                for i in range(2, n + 1):
+                    resultado *= i
+            self._memoria[n] = resultado
+            return resultado
+
+
+calc1 = SingletonFactorial()
+calc2 = SingletonFactorial()
+print(calc1 is calc2)
+print(calc1.factorial(5))
+print(calc2.factorial(6))
